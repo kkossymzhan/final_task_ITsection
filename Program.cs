b6c6842f90a165e61807987b4bcbcd664693369f
kkossymzhan <gulim.kossymzhan@nu.edu.kz>
2023-05-19 18:19:04 +0600

    Creating first array

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/* Написать программу, которая из имеющегося 
+массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При 
+решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+string[] inputWords = new string [5];
+string[] electedWords = new string [inputWords.Length];
+
+for (int i = 0; i < inputWords.Length; i++)
+{
+    inputWords[i]= Console.ReadLine();
+}
+
+void PrintArray(string[] array)
+{
+    for (int i =0; i < array.Length; i++)
+        System.Console.Write(array[i] + " ");
+}
+
+System.Console.WriteLine("Initial words:");
+PrintArray(inputWords);
